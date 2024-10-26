5311bbe46791fa7e1409e1e67fc701a0fda51e99
Daniel Udekwe <dau24@fsu.edu>
2024-10-26 11:40:57 -0400

    first commit

---
+++ Assets/My Scripts/PlayerInteraction.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float playerReach = 3f;
+    Interactable currentInteractable;
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteraction();
+        if (Input.GetKeyDown(KeyCode.F) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+
+    void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        //if colliders with anything within player reach
+        if (Physics.Raycast(ray, out hit, playerReach))
+        {
+            if (hit.collider.tag == "Interactable") // if looking at an interactable object.
+            {
+                Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+                // if there is a currentInteractalbe and it is not the newInteractable
+                if (currentInteractable && newInteractable != currentInteractable)
+                {
+                    currentInteractable.DisableOutline();
+                }
+                if (newInteractable.enabled)
+                {
+                    SetNewCurrentInteractable(newInteractable);
+                }
+                else // if new interactable is not enabled
+                {
+                    DisableCurrentInteractable();
+                }
+            }
+            else //if not an interactable
+            {
+                DisableCurrentInteractable();
+            }
+
+        }
+        else // if nothing in reach
+        {
+            DisableCurrentInteractable();
+        }
+    }
+    void SetNewCurrentInteractable(Interactable newInteractable)
+    {
+        currentInteractable = newInteractable;
+        currentInteractable.EnableOutline();
+        HUDController.instance.EnableInteractionText(currentInteractable.message);
+    }
+
+    void DisableCurrentInteractable()
+    {
+        HUDController.instance.DisableInteractionText();
+        if (currentInteractable)
+        {
+            currentInteractable.DisableOutline();
+            currentInteractable = null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Surveying Lab for Engineering
+
+## Add this file from the unity asset store "Unity Terrain - URP Demo Scene"
+
+![Alt text](Demos/home.png)
+<br/> Virtual Lab Landing Page. 
